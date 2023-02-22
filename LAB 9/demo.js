ec3ac38069d0c3374442aa58c22fef1b6b572107
D22IT184 <117455872+D22IT184@users.noreply.github.com>
2023-02-22 11:07:44 +0530

    demo.js

---
+++ LAB 9/demo.js
@@ -0,0 +1,39 @@
+let studentArr = [
+    {
+        name:"Ritesh",
+        age:20,
+        ID:"20it001",
+        WT_Marks:58,
+        COMI_Marks:78,
+        CN_Marks:65
+    },
+    {
+        name:"Mitesh",
+        age:21,
+        ID:"20it002",
+        WT_Marks:68,
+        COMI_Marks:58,
+        CN_Marks:85
+    },
+    {
+        name:"Hitesh",
+        age:22,
+        ID:"20it003",
+        WT_Marks:88,
+        COMI_Marks:88,
+        CN_Marks:95
+    },
+    {
+        name:"Pritesh",
+        age:23,
+        ID:"20it004",
+        WT_Marks:98,
+        COMI_Marks:88,
+        CN_Marks:95
+    },
+]
+
+
+let a  = studentArr.forEach(function(s,i){
+    console.log()
+})
