cb4d23652837f29b9fc08c9f012e88bdabadec21
D22IT184 <117455872+D22IT184@users.noreply.github.com>
2023-03-01 08:07:11 +0530

    for.js

---
+++ Practical 8/for.js
@@ -0,0 +1,52 @@
+function validateForm() {
+  // Get values of form elementsvar userid = document.forms["myForm"]["userid"].value;
+  var password = document.forms["myForm"]["password"].value;
+  var name = document.forms["myForm"]["name"].value;
+  var address = document.forms["myForm"]["address"].value;
+  var country = document.forms["myForm"]["country"].value;
+  var zipcode = document.forms["myForm"]["zipcode"].value;
+  var email = document.forms["myForm"]["email"].value;
+  var sex = document.forms["myForm"]["sex"].value;
+  var english = document.forms["myForm"]["english"].checked;
+  var non_english = document.forms["myForm"]["non_english"].checked;
+  var about = document.forms["myForm"]["about"].value;
+  // Validate userid
+  if (userid == "" || userid.length < 5 || userid.length > 12) {
+  alert("Userid must be between 5 and 12 characters");
+  return false;
+  }
+  // Validate password
+  if (password == "" || password.length < 7 || password.length > 12) {
+  alert("Password must be between 7 and 12 characters");
+  return false;
+  }
+  // Validate name
+  if (name == "" || !/^[a-zA-Z]+$/.test(name)) {
+  alert("Name can only contain alphabets");
+  return false;}
+  // Validate country
+  if (country == "") {
+  alert("Please select a country");
+  return false;
+  }
+  if (zipcode == "" || isNaN(zipcode)) {
+  alert("Zipcode must be a number");
+  return false;
+  }
+  // Validate email
+  if (email == "") {
+  alert("Email is required");
+  return false;
+  }
+  // Validate sex
+  if (sex == "") {
+  alert("Please select a sex");
+  return false;
+  }
+  // Validate language
+  if (!english || !non_english) {
+  alert("Please select at least one language");return false;
+  }
+  // All validations passed
+  return true;
+  }
